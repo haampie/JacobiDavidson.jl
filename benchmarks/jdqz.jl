ab7b06f9b21690f5446cd80c720da52a508812f9
Harmen Stoppels <harmenstoppels@gmail.com>
2017-07-04 11:36:02 +0200

    Profiling on

--- benchmarks/jdqz.jl
+++ benchmarks/jdqz.jl
@@ -2,7 +2,7 @@ module JDQZBench
 
 using JacobiDavidson
 
-function bench_jdqz_allocs(; n = 100, τ = 2.0 + 0.01im)
+function bench_jdqz_allocs(; n = 1_000, τ = 2.0 + 0.01im)
   srand(4)
   
   A = 100 * speye(Complex128, n) + sprand(Complex128, n, n, .5)
@@ -19,23 +19,11 @@ function bench_jdqz_allocs(; n = 100, τ = 2.0 + 0.01im)
     ɛ = 1e-8,
     τ = τ
   )
-
-  Profile.clear_malloc_data()
-
-  srand(4)
-
-  result = jdqz(
-    A,
-    B,
-    exact_solver(),
-    pairs = 10,
-    min_dimension = 10,
-    max_dimension = 15,
-    max_iter = 300,
-    ɛ = 1e-8,
-    τ = τ
-  )
 end
+
+
 end
 
 JDQZBench.bench_jdqz_allocs()
+Profile.clear()
+@profile JDQZBench.bench_jdqz_allocs()
